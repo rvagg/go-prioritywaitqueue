228fb101dbd7214b0720e7408bc0cd2e54253800
Rod Vagg <rod@vagg.org>
2023-04-03 16:16:56 +1000

    feat: add WithInitialPause option

--- prioritywaitqueue.go
+++ prioritywaitqueue.go
@@ -5,6 +5,7 @@ package prioritywaitqueue
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // PriorityWaitQueue is a blocking queue for coordinating goroutines, providing
@@ -23,50 +24,86 @@ type PriorityWaitQueue[T interface{}] interface {
 	Wait(waitWith T) func()
 }
 
+type Option[T interface{}] func(PriorityWaitQueue[T])
+
+// WithInitialPause sets an initial pause for the first call to Wait() on the
+// PriorityWaitQueue. This is useful if you want to allow goroutines to
+// queue up before any of them are allowed to run. A short pause will mean that
+// the initial first-in-first-run behaviour is overridden, where the first
+// goroutine may have to compete with others before getting to run.
+func WithInitialPause[T interface{}](duration time.Duration) Option[T] {
+	return func(q PriorityWaitQueue[T]) {
+		q.(*priorityWaitQueue[T]).initialPause = duration
+	}
+}
+
 // ComparePriority should return true if a has a higher priority, and therefore
 // should run, BEFORE b.
 type ComparePriority[T interface{}] func(a T, b T) bool
 
 // New creates a new PriorityWaitQueue with the provided ComparePriority
 // function for type T.
-func New[T interface{}](cmp ComparePriority[T]) PriorityWaitQueue[T] {
-	return &priorityWaitQueue[T]{
+func New[T interface{}](cmp ComparePriority[T], options ...Option[T]) PriorityWaitQueue[T] {
+	pwq := &priorityWaitQueue[T]{
 		cmp:     cmp,
 		cond:    sync.NewCond(&sync.Mutex{}),
 		waiters: make([]*T, 0),
 	}
+	for _, opt := range options {
+		opt(pwq)
+	}
+	return pwq
 }
 
 var _ PriorityWaitQueue[int] = &priorityWaitQueue[int]{}
 
 type priorityWaitQueue[T interface{}] struct {
-	cmp     ComparePriority[T]
-	cond    *sync.Cond
-	waiters []*T
-	running *T
+	cmp              ComparePriority[T]
+	cond             *sync.Cond
+	waiters          []*T
+	running          *T
+	initialPause     time.Duration
+	initialPauseDone bool
+	initialPauseLk   sync.Mutex
 }
 
-func (brq *priorityWaitQueue[T]) Wait(waitWith T) func() {
+func (pwq *priorityWaitQueue[T]) Wait(waitWith T) func() {
 	waitWithPtr := &waitWith
-	brq.cond.L.Lock()
-	defer brq.cond.L.Unlock()
+
+	pwq.cond.L.Lock()
+	defer pwq.cond.L.Unlock()
+
 	// register us as a waiter
-	brq.waiters = append(brq.waiters, waitWithPtr)
+	pwq.waiters = append(pwq.waiters, waitWithPtr)
+
+	// if we have an initial pause, do it now and block any subsequent calls since
+	// we (the first caller) hold the lock
+	if pwq.initialPause > 0 {
+		pwq.cond.L.Unlock()
+		pwq.initialPauseLk.Lock()
+		if !pwq.initialPauseDone {
+			time.Sleep(pwq.initialPause)
+			pwq.initialPauseDone = true
+		}
+		pwq.initialPauseLk.Unlock()
+		pwq.cond.L.Lock()
+	}
+
 	for {
-		if brq.running == nil { // none currently running, check if we can
+		if pwq.running == nil { // none currently running, check if we can
 			canRun := true
 			// is there another waiter in the queue with higher priority than us?
-			for _, waiter := range brq.waiters {
-				if waiter != waitWithPtr && brq.cmp(*waiter, waitWith) {
+			for _, waiter := range pwq.waiters {
+				if waiter != waitWithPtr && pwq.cmp(*waiter, waitWith) {
 					canRun = false
 				}
 			}
 			if canRun { // didn't find a higher-priority waiter, we can run
 				// remove us from the wait list
 				removed := false
-				for i, waiter := range brq.waiters {
+				for i, waiter := range pwq.waiters {
 					if waiter == waitWithPtr {
-						brq.waiters = append(brq.waiters[:i], brq.waiters[i+1:]...)
+						pwq.waiters = append(pwq.waiters[:i], pwq.waiters[i+1:]...)
 						removed = true
 						break
 					}
@@ -74,23 +111,24 @@ func (brq *priorityWaitQueue[T]) Wait(waitWith T) func() {
 				if !removed {
 					panic("didn't find current waiter in the wait list")
 				}
-				brq.running = waitWithPtr
+				pwq.running = waitWithPtr
 				// done() must be called when the work is complete and another job
 				// can be run
 				done := func() {
-					brq.cond.L.Lock()
-					defer brq.cond.L.Unlock()
-					if brq.running != waitWithPtr {
-						panic(fmt.Sprintf("Done() was called with a runner that was not expected to be running: %v <> %v", brq.running, &waitWith))
+					pwq.cond.L.Lock()
+					defer pwq.cond.L.Unlock()
+					if pwq.running != waitWithPtr {
+						panic(fmt.Sprintf("Done() was called with a runner that was not expected to be running: %v <> %v", pwq.running, &waitWith))
 					}
-					brq.running = nil
+					pwq.running = nil
 					// notify all to check whether they are next to run
-					brq.cond.Broadcast()
+					pwq.cond.Broadcast()
 				}
 				return done
 			}
 		}
-		// wait until we get a broadcast
-		brq.cond.Wait()
+
+		// unlock and wait until we get a broadcast
+		pwq.cond.Wait()
 	}
 }
